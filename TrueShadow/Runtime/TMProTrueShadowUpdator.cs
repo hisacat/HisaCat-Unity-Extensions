97a6487c51879c62c1c72ad08b367f092b2de922
HisaCat <ahisacat@gmail.com>
2025-09-18 22:26:28 +0900

    feat: TrueShadow

---
+++ TrueShadow/Runtime/TMProTrueShadowUpdator.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using TMPro;
+using LeTai.TrueShadow;
+using HisaCat.PropertyAttributes;
+using UnityEngine.UI;
+using System.Collections.Generic;
+
+namespace HisaCat.HUE
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(TextMeshProUGUIEx))]
+    [RequireComponent(typeof(TrueShadow))]
+    public class TMProTrueShadowUpdator : MonoBehaviour
+    {
+        [ReadOnly][SerializeField] private TextMeshProUGUIEx m_TextMeshProUGUIEx = null;
+        [ReadOnly][SerializeField] private TrueShadow m_TrueShadow = null;
+        private HashSet<TMP_SubMeshUI> knownSubMeshes = new();
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if (Application.isPlaying) return;
+
+            this.m_TextMeshProUGUIEx = this.GetComponent<TextMeshProUGUIEx>();
+            this.m_TrueShadow = this.GetComponent<TrueShadow>();
+        }
+#endif
+        private void Awake()
+        {
+            this.m_TextMeshProUGUIEx.OnPostPopulateMesh -= OnPostPopulateMesh;
+            this.m_TextMeshProUGUIEx.OnPostPopulateMesh += OnPostPopulateMesh;
+
+            CopyToTMPSubMeshesIfRequired();
+        }
+        private void OnDestroy()
+        {
+            this.m_TextMeshProUGUIEx.OnPostPopulateMesh -= OnPostPopulateMesh;
+        }
+
+        private void OnPostPopulateMesh(VertexHelper vh) => CopyToTMPSubMeshesIfRequired();
+
+        private void CopyToTMPSubMeshesIfRequired()
+        {
+            var subMeshObjects = this.m_TextMeshProUGUIEx.GetSubMeshObjects();
+            foreach (var subMeshObject in subMeshObjects)
+            {
+                if (subMeshObject == null) continue;
+                if (this.knownSubMeshes.Contains(subMeshObject)) continue;
+
+                this.knownSubMeshes.Add(subMeshObject);
+                this.m_TrueShadow.CopyTo(subMeshObject.gameObject);
+            }
+
+            this.knownSubMeshes.RemoveWhere(IsNull);
+            static bool IsNull(TMP_SubMeshUI subMesh) => subMesh == null;
+        }
+    }
+}
