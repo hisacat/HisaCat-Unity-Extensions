2edee5cad7bf666d35ffdf4fa187db36c89978b1
HisaCat <ahisacat@gmail.com>
2025-09-19 02:13:19 +0900

    chore: ReadOnlyHashSet single-line style

--- Core/Runtime/Collections/ReadOnlyHashSet.cs
+++ Core/Runtime/Collections/ReadOnlyHashSet.cs
@@ -12,35 +12,17 @@ namespace HisaCat.HUE.Collections
             _data = data;
         }
 
-        public int Count
-            => this._data.Count;
+        public int Count => this._data.Count;
+        public bool Contains(T item) => this._data.Contains(item);
+        public IEnumerator<T> GetEnumerator() => this._data.GetEnumerator();
+        public bool IsProperSubsetOf(IEnumerable<T> other) => this._data.IsProperSubsetOf(other);
+        public bool IsProperSupersetOf(IEnumerable<T> other) => this._data.IsProperSupersetOf(other);
+        public bool IsSubsetOf(IEnumerable<T> other) => this._data.IsSubsetOf(other);
+        public bool IsSupersetOf(IEnumerable<T> other) => this._data.IsSupersetOf(other);
+        public bool Overlaps(IEnumerable<T> other) => this._data.Overlaps(other);
+        public bool SetEquals(IEnumerable<T> other) => this._data.SetEquals(other);
 
-        public bool Contains(T item)
-            => this._data.Contains(item);
-
-        public IEnumerator<T> GetEnumerator()
-            => this._data.GetEnumerator();
-
-        public bool IsProperSubsetOf(IEnumerable<T> other)
-            => this._data.IsProperSubsetOf(other);
-
-        public bool IsProperSupersetOf(IEnumerable<T> other)
-            => this._data.IsProperSupersetOf(other);
-
-        public bool IsSubsetOf(IEnumerable<T> other)
-            => this._data.IsSubsetOf(other);
-
-        public bool IsSupersetOf(IEnumerable<T> other)
-            => this._data.IsSupersetOf(other);
-
-        public bool Overlaps(IEnumerable<T> other)
-            => this._data.Overlaps(other);
-
-        public bool SetEquals(IEnumerable<T> other)
-            => this._data.SetEquals(other);
-
-        IEnumerator IEnumerable.GetEnumerator()
-            => this.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
     }
 
     public interface IReadOnlyHashSet<T> : IReadOnlyCollection<T>
@@ -56,13 +38,11 @@ namespace HisaCat.HUE.Collections
         bool SetEquals(IEnumerable<T> other);
     }
 
-    public class ReadOnlyableHashSet<T> : HashSet<T>, IReadOnlyHashSet<T>
-    {
-    }
+    public class ReadOnlyableHashSet<T> : HashSet<T>, IReadOnlyHashSet<T> { }
 
     public static class ReadOnlyHashSetExtensions
     {
-        public static IReadOnlyHashSet<T> AsReadOnly<T>(this ISet<T> set)
-            => new ReadOnlyHashSet<T>(set);
+        public static ReadOnlyHashSet<T> AsReadOnly<T>(this ISet<T> set)
+            => new(set);
     }
 }
