994fc70aa4c94d3e9efeadf8c9ce818ab16b8754
HisaCat <ahisacat@gmail.com>
2026-01-12 22:50:47 +0900

    feat: QualitySettingsHelper

---
+++ Core/Runtime/UnityExtensions/HisaCat/UnityExtensions/Helpers/QualitySettingsHelper.cs
@@ -0,0 +1,19 @@
+
+using UnityEngine;
+
+namespace HisaCat.UnityExtensions
+{
+    public static class QualitySettingsHelper
+    {
+        public enum VSyncCount
+        {
+            DontSync = 0,
+            EveryVBlank = 1,
+            EverySecondVBlank = 2,
+        }
+        public static void SetVSyncCount(VSyncCount vSyncCount)
+        {
+            QualitySettings.vSyncCount = (int)vSyncCount;
+        }
+    }
+}
