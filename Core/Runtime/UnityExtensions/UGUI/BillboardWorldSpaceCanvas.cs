98516328fc5d4a92b87803eaf54a15564ea7408f
HisaCat <ahisacat@gmail.com>
2026-01-23 19:16:05 +0900

    feat: BillboardWorldSpaceCanvas

---
+++ Core/Runtime/UnityExtensions/UGUI/BillboardWorldSpaceCanvas.cs
@@ -0,0 +1,32 @@
+using HisaCat.PropertyAttributes;
+using HisaCat.UnityExtensions;
+using UnityEngine;
+
+namespace HisaCat.UGUI
+{
+    [ExecuteAlways]
+    [RequireComponent(typeof(Canvas))]
+    public class BillboardWorldSpaceCanvas : MonoBehaviour
+    {
+        [ReadOnly][SerializeField] private Canvas m_Canvas = null;
+        [SerializeField] private Quaternion m_Rotation = Quaternion.identity;
+        private void Awake()
+        {
+            if (this.ConditionLogWarning(this.m_Canvas == null,
+            $"[{nameof(BillboardWorldSpaceCanvas)}] {nameof(Canvas)} is not assigned! it will be assigned automatically.", this))
+                this.m_Canvas = GetComponent<Canvas>();
+        }
+
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            this.m_Canvas = GetComponent<Canvas>();
+        }
+#endif
+
+        private void LateUpdate()
+        {
+            this.transform.rotation = Camera.main.transform.rotation * this.m_Rotation;
+        }
+    }
+}
