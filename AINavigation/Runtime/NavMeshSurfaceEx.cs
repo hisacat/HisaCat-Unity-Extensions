f4880651548bc901eb4cd28be3481ae27413c3be
HisaCat <ahisacat@gmail.com>
2026-01-29 00:27:10 +0900

    feat: [WIP] Write NavMeshSurfaceEx

---
+++ AINavigation/Runtime/NavMeshSurfaceEx.cs
@@ -0,0 +1,12 @@
+using System.Collections.Generic;
+using Unity.AI.Navigation;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace HisaCat.UnityExtensions.AI.Navigation
+{
+    public class NavMeshSurfaceEx : NavMeshSurface
+    {
+        // TODO: Build NavMesh Aync
+    }
+}
