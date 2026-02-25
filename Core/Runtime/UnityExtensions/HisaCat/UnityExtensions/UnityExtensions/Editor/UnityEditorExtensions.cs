b39937511cf8fff6c803125c09be5d8d7e2e8ef2
HisaCat <ahisacat@gmail.com>
2026-02-25 23:16:26 +0900

    feat: SerializePropertyExtensions

--- Core/Runtime/UnityExtensions/HisaCat/UnityExtensions/UnityExtensions/Editor/UnityEditorExtensions.cs
+++ Core/Runtime/UnityExtensions/HisaCat/UnityExtensions/UnityExtensions/Editor/UnityEditorExtensions.cs
@@ -1,4 +1,5 @@
 #if UNITY_EDITOR
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using UnityEditor;
 using UnityEngine;
@@ -85,6 +86,87 @@ namespace HisaCat.HUE.UnityExtensions.Editors
             var guid = AssetDatabase.AssetPathToGUID(assetPath);
             EditorGUIUtility.systemCopyBuffer = guid;
             Debug.Log($"[{nameof(CopyAssetType)}]: Copied \"{target.name}\"s GUID \"{guid}\" to clipboard.");
+        }
+    }
+
+    public static class SerializePropertyExtensions
+    {
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T GetEnumValue<T>(this SerializedProperty enumArrayProperty) where T : System.Enum
+        {
+            return (T)System.Enum.ToObject(typeof(T), enumArrayProperty.intValue);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetEnumValue<T>(this SerializedProperty enumArrayProperty, T value) where T : System.Enum
+        {
+            enumArrayProperty.intValue = (int)System.Enum.Parse(typeof(T), value.ToString());
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static T[] GetArrayEnumElements<T>(this SerializedProperty enumArrayProperty) where T : System.Enum
+        {
+            var array = new T[enumArrayProperty.arraySize];
+            for (int i = 0; i < enumArrayProperty.arraySize; i++)
+            {
+                var element = enumArrayProperty.GetArrayElementAtIndex(i);
+                array[i] = element.GetEnumValue<T>();
+            }
+            return array;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetArrayEnumElements<T>(this SerializedProperty enumArrayProperty, IEnumerable<T> values) where T : System.Enum
+        {
+            enumArrayProperty.ClearArray();
+            foreach (var value in values)
+                enumArrayProperty.AddArrayEnumElement(value);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddArrayEnumElement<T>(this SerializedProperty enumArrayProperty, T value) where T : System.Enum
+        {
+            enumArrayProperty.arraySize++;
+            var element = enumArrayProperty.GetArrayElementAtIndex(enumArrayProperty.arraySize - 1);
+            element.SetEnumValue(value);
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void InsertArrayEnumElement<T>(this SerializedProperty enumArrayProperty, int index, T value) where T : System.Enum
+        {
+            enumArrayProperty.InsertArrayElementAtIndex(index);
+            var element = enumArrayProperty.GetArrayElementAtIndex(index);
+            element.SetEnumValue(value);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static SerializedProperty[] GetArrayElements(this SerializedProperty arrayProperty)
+        {
+            var elements = new SerializedProperty[arrayProperty.arraySize];
+            for (int i = 0; i < arrayProperty.arraySize; i++)
+            {
+                var element = arrayProperty.GetArrayElementAtIndex(i);
+                elements[i] = element;
+            }
+            return elements;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int[] GetIntArrayElements(this SerializedProperty intArrayProperty)
+        {
+            var array = new int[intArrayProperty.arraySize];
+            for (int i = 0; i < intArrayProperty.arraySize; i++)
+            {
+                var element = intArrayProperty.GetArrayElementAtIndex(i);
+                array[i] = element.intValue;
+            }
+            return array;
+        }
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetIntArrayElements(this SerializedProperty intArrayProperty, IEnumerable<int> values)
+        {
+            intArrayProperty.ClearArray();
+            foreach (var value in values)
+            {
+                intArrayProperty.arraySize++;
+                var element = intArrayProperty.GetArrayElementAtIndex(intArrayProperty.arraySize - 1);
+                element.intValue = value;
+            }
         }
     }
 }
